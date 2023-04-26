ed0c5c28198f5b534ac83d8ad9efd4c6a120d0bd
Manish-456 <tamangmanish3456@gmail.com>
2023-04-26 14:22:56 +0545

    API_URL CHANGED

--- client/src/context/socketContext.jsx
+++ client/src/context/socketContext.jsx
@@ -1,13 +1,14 @@
 import { createContext, useEffect, useState } from "react";
 import io from "socket.io-client";
+import { API_URL } from "../config";
 
 export const SocketContext = createContext();
 
 export const SocketContextProvider = ({ children }) => {
   const [socket, setSocket] = useState(null);
-
+ 
   useEffect(() => {
-    const newSocket = io(`http://localhost:5000`);
+    const newSocket = io(API_URL);
     setSocket(newSocket);
 
     return () => newSocket.close();
